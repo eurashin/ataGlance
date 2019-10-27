a27a5c88b66579ef201fa5ecb4738f611ef16cd5
eurashin <eura.shin@gmail.com>
2019-10-26 23:23:21 -0400

    hi

---
+++ api/tools.py
@@ -0,0 +1,15 @@
+def createSID(tags):
+    client = datastore.Client()
+
+    storyID = ""
+
+    for tag in tags:
+        # query the tag table for all the tag IDs
+        query = client.query(kind='Tag')
+        query.add_filter('tagName', '=', tag)
+        tagID = query.fetch()
+
+        # concat the tag IDs together to create the SID
+        storyID = storyID + "-" + tagID
+        
+    return storyID
